aa7639516b70fa64368bfbe1b82fea2bd2b04626
1393131688 <1393131688@qq.com>
2024-02-12 00:35:31 +0800

    Update LeafDataset.py
    Added description of functions

--- Dataset/LeafDataset.py
+++ Dataset/LeafDataset.py
@@ -5,6 +5,12 @@ import os
 #Loading Palm Leaf Manuscript Dataset
 class LeafDataset(Dataset):
     def __init__(self, root_dir, transform=None):
+        '''
+        Args:
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        '''
         self.root_dir = root_dir
         self.transform = transform
         self.imgs = []
@@ -16,9 +22,11 @@ class LeafDataset(Dataset):
                 self.imgs.append((img_path, class_name))
 
     def __len__(self):
+        '''Returns the total number of image files'''
         return len(self.imgs)
 
     def __getitem__(self, idx):
+        '''Returns the image and its class label'''
         img_path, class_name = self.imgs[idx]
         img = cv2.imread(img_path)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
